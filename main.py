830808b88b580da49f88648ee82636aea02c671a
BAL17 <126348686+BAL17@users.noreply.github.com>
2024-04-26 14:00:18 -0500

    flask api up and running with simple get res

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ app=Flask(__name__)
 api=Api(app)
 
 
+class HelloWorld(Resource):
+    def get(self):
+        return ("data:" "Hello World")
+            
+api.add_resource(HelloWorld, "/helloworld") 
+
+
 if __name__ == "__main__":
     app.run(debug = True) #only for development env turn off for prod
 
